03e43098aff1fa7060392494fecf49ac866b0783
sowish23 <soso2266@naver.com>
2022-02-04 11:30:05 +0900

    add oximetry comment

--- aws/lambda/oximetry_api_py.py
+++ aws/lambda/oximetry_api_py.py
@@ -3,14 +3,20 @@ import requests
 import datetime
 from pytz import timezone
 
+# oximetry 들어오는 값들을 처리하기 위한 코드
+
 def lambda_handler(event, context):
-    # TODO implement
+    # date 저장을 위한 변수
     now = datetime.datetime.now(timezone('Asia/Seoul'))
     nowDatetime = now.strftime('%Y-%m-%dT%H:%M:%S')
     
     url = ''    # set opensearch endpoint
+    
+    # oximetry 로 받은 값을 json화 시키고 현재 시간 값 넣어서 post
     data = json.loads(event['body'])
     data['date'] = nowDatetime
     headers = {'Content-type': 'application/json'}
+    
+    # auth값은 헤더의 값 참조
     response = requests.post(url, data = json.dumps(data), headers=headers, auth=('crc', event['headers']['crc'])).json()
     return response
